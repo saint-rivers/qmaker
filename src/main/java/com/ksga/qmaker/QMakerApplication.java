e13792270ca0e71d0f07084cd4978ebfaecf36ad
Dayan Eam <eam.dayan@gmail.com>
2022-03-18 14:06:29 +0700

    first commit

---
+++ src/main/java/com/ksga/qmaker/QMakerApplication.java
@@ -0,0 +1,13 @@
+package com.ksga.qmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksga/qmaker/QmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksga.qmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
